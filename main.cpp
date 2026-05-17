38574d7be3d448ccb6fe2a8cbdd218abf6a6a14a
wojtuswowo <wowodwa@gmail.com>
2026-05-17 20:45:02 +0200

    dodane zostało: kontrola przepływu(1.3), terminator(1.5), PING(5), nasłuch ciągły

--- main.cpp
+++ main.cpp
@@ -3,9 +3,80 @@
 #include <sstream>
 #include <winbase.h>
 #include <fstream>
+#include <conio.h>
 
 #define PLIK_USTAWIEN_TRANSMISJI "TransConfig.config"
 #define PLIK_USTAWIEN_TIMEOUTOW "TimeOutConfig.config"
+
+// Kody terminatorow
+#define TERM_BRAK    0
+#define TERM_CR      1
+#define TERM_LF      2
+#define TERM_CRLF    3
+#define TERM_WLASNY  4
+
+// Kody kontroli przeplywu
+#define FLOW_BRAK    0
+#define FLOW_RTSCTS  1
+#define FLOW_DTRDSR  2
+#define FLOW_XONXOFF 3
+
+// Ustawienia terminatora i przeplywu globalne (zapisywane razem z DCB)
+struct UstawieniaPort {
+    int terminatorTyp;       // TERM_*
+    char terminatorZnak1;    // 1. znak terminatora wlasnego
+    char terminatorZnak2;    // 2. znak terminatora wlasnego (0 = brak)
+    int kontrolaPrzeplywu;   // FLOW_*
+};
+
+// Daje string z terminatorem do dolaczenia do wiadomosci
+std::string pobierzTerminator(UstawieniaPort uPr)
+{
+    std::string term = "";
+    switch(uPr.terminatorTyp)
+    {
+        case TERM_CR:     term += '\r'; break;
+        case TERM_LF:     term += '\n'; break;
+        case TERM_CRLF:   term += '\r'; term += '\n'; break;
+        case TERM_WLASNY:
+            if(uPr.terminatorZnak1) term += uPr.terminatorZnak1;
+            if(uPr.terminatorZnak2) term += uPr.terminatorZnak2;
+            break;
+        case TERM_BRAK:
+        default: break;
+    }
+    return term;
+}
+
+// Wyswietla czytelny opis terminatora
+void wyswietlTerminator(UstawieniaPort uPr)
+{
+    switch(uPr.terminatorTyp)
+    {
+        case TERM_BRAK:   std::cout<<"brak"; break;
+        case TERM_CR:     std::cout<<"CR (\\r)"; break;
+        case TERM_LF:     std::cout<<"LF (\\n)"; break;
+        case TERM_CRLF:   std::cout<<"CR-LF (\\r\\n)"; break;
+        case TERM_WLASNY:
+            std::cout<<"wlasny [";
+            if(uPr.terminatorZnak1) std::cout<<"0x"<<std::hex<<(int)(unsigned char)uPr.terminatorZnak1<<std::dec;
+            if(uPr.terminatorZnak2) std::cout<<" 0x"<<std::hex<<(int)(unsigned char)uPr.terminatorZnak2<<std::dec;
+            std::cout<<"]";
+            break;
+    }
+}
+
+// Wyswietla czytelny opis kontroli przeplywu
+void wyswietlKontrolePrzeplywu(UstawieniaPort uPr)
+{
+    switch(uPr.kontrolaPrzeplywu)
+    {
+        case FLOW_BRAK:    std::cout<<"brak"; break;
+        case FLOW_RTSCTS:  std::cout<<"sprzet. RTS/CTS"; break;
+        case FLOW_DTRDSR:  std::cout<<"sprzet. DTR/DSR"; break;
+        case FLOW_XONXOFF: std::cout<<"programowa XON/XOFF"; break;
+    }
+}
 
 /*
 DODAĆ ZAPIS USTAWIEN DO PLIKU
@@ -396,6 +467,188 @@ DCB ustawieniaTransmisji(DCB dcbParam)
     return dcbParam;
 }
 
+// Aplikuje ustawienia kontroli przeplywu do struktury DCB
+void zastosujKontrolePrzeplywu(DCB &dcb, int typPrzeplywu)
+{
+    // Zerujemy wszystkie flagi przeplywu przed ustawieniem nowych
+    dcb.fOutxCtsFlow = FALSE;
+    dcb.fOutxDsrFlow = FALSE;
+    dcb.fDtrControl  = DTR_CONTROL_DISABLE;
+    dcb.fRtsControl  = RTS_CONTROL_DISABLE;
+    dcb.fOutX        = FALSE;
+    dcb.fInX         = FALSE;
+
+    switch(typPrzeplywu)
+    {
+        case FLOW_RTSCTS:
+            // Sprzętowy handshake RTS/CTS
+            dcb.fOutxCtsFlow = TRUE;
+            dcb.fRtsControl  = RTS_CONTROL_HANDSHAKE;
+            break;
+        case FLOW_DTRDSR:
+            // Sprzętowy handshake DTR/DSR
+            dcb.fOutxDsrFlow = TRUE;
+            dcb.fDtrControl  = DTR_CONTROL_HANDSHAKE;
+            break;
+        case FLOW_XONXOFF:
+            // Programowy XON/XOFF (domyslne znaki 0x11/0x13)
+            dcb.fOutX = TRUE;
+            dcb.fInX  = TRUE;
+            dcb.XonChar  = 0x11;
+            dcb.XoffChar = 0x13;
+            dcb.XonLim   = 100;
+            dcb.XoffLim  = 100;
+            break;
+        case FLOW_BRAK:
+        default:
+            // Bez kontroli przeplywu - DTR ustawiamy na enable zeby port dzialal normalnie
+            dcb.fDtrControl = DTR_CONTROL_ENABLE;
+            dcb.fRtsControl = RTS_CONTROL_ENABLE;
+            break;
+    }
+}
+
+// Podmenu wyboru terminatora
+UstawieniaPort ustawTerminator(UstawieniaPort uPr)
+{
+    std::cout<<"\nWybierz terminator wiadomosci:\n"
+             <<"  0 - brak terminatora\n"
+             <<"  1 - CR (\\r)\n"
+             <<"  2 - LF (\\n)\n"
+             <<"  3 - CR-LF (\\r\\n)\n"
+             <<"  4 - wlasny (1 lub 2 znaki hex)\n"
+             <<"Obecny: "; wyswietlTerminator(uPr); std::cout<<"\nWybor> ";
+    char wyb;
+    std::cin>>wyb;
+    switch(wyb)
+    {
+        case '0': uPr.terminatorTyp = TERM_BRAK;   break;
+        case '1': uPr.terminatorTyp = TERM_CR;     break;
+        case '2': uPr.terminatorTyp = TERM_LF;     break;
+        case '3': uPr.terminatorTyp = TERM_CRLF;   break;
+        case '4':
+            uPr.terminatorTyp = TERM_WLASNY;
+            // Pobieramy 1 lub 2 bajty w hex
+            std::cout<<"Podaj 1. znak terminatora (hex, np. 0D): ";
+            unsigned int tmp;
+            std::cin>>std::hex>>tmp>>std::dec;
+            uPr.terminatorZnak1 = (char)tmp;
+            std::cout<<"Podaj 2. znak (hex, 00 = brak): ";
+            std::cin>>std::hex>>tmp>>std::dec;
+            uPr.terminatorZnak2 = (char)tmp;
+            break;
+        default:
+            std::cout<<"Nieznany wybor, terminator nie zmieniony.\n";
+    }
+    return uPr;
+}
+
+// Podmenu wyboru kontroli przeplywu
+UstawieniaPort ustawKontrolePrzeplywu(UstawieniaPort uPr)
+{
+    std::cout<<"\nWybierz kontrole przeplywu:\n"
+             <<"  0 - brak\n"
+             <<"  1 - sprzętowa RTS/CTS\n"
+             <<"  2 - sprzętowa DTR/DSR\n"
+             <<"  3 - programowa XON/XOFF\n"
+             <<"Obecna: "; wyswietlKontrolePrzeplywu(uPr); std::cout<<"\nWybor> ";
+    char wyb;
+    std::cin>>wyb;
+    switch(wyb)
+    {
+        case '0': uPr.kontrolaPrzeplywu = FLOW_BRAK;    break;
+        case '1': uPr.kontrolaPrzeplywu = FLOW_RTSCTS;  break;
+        case '2': uPr.kontrolaPrzeplywu = FLOW_DTRDSR;  break;
+        case '3': uPr.kontrolaPrzeplywu = FLOW_XONXOFF; break;
+        default:
+            std::cout<<"Nieznany wybor, kontrola przeplywu nie zmieniona.\n";
+    }
+    return uPr;
+}
+
+// PING - wysyla krotka wiadomosc i mierzy czas odpowiedzi (round trip delay)
+void ping(int nrPortu, DCB parametry, COMMTIMEOUTS timeouts, UstawieniaPort uPr)
+{
+    // Na PING uzywamy duzego timeoutu odpowiedzi - uzytkownik moze ustawic ile czekac
+    std::cout<<"\nPodaj timeout oczekiwania na odpowiedz PING [ms, 0-10000]: ";
+    int pingTimeout = pobierzIntWPrzedziale(0, 10000);
+
+    HANDLE hSzeregowy = utworzPort(nrPortu);
+    if(!hSzeregowy) return;
+
+    zastosujKontrolePrzeplywu(parametry, uPr.kontrolaPrzeplywu);
+
+    if(!SetCommState(hSzeregowy, &parametry))
+    {
+        std::cout<<"BLAD: Nie udalo sie ustawic parametrow portu.\n";
+        wyswietlBlad();
+        CloseHandle(hSzeregowy);
+        return;
+    }
+
+    // Timeout dla pinga - ReadTotalTimeoutConstant = ile ms czekamy na odpowiedz
+    COMMTIMEOUTS pingTime = timeouts;
+    pingTime.ReadTotalTimeoutConstant  = pingTimeout;
+    pingTime.ReadTotalTimeoutMultiplier = 1;
+    pingTime.ReadIntervalTimeout = 50; // 50ms miedzy znakami odpowiedzi
+
+    if(!SetCommTimeouts(hSzeregowy, &pingTime))
+    {
+        std::cout<<"BLAD: Nie udalo sie ustawic timeoutow.\n";
+        wyswietlBlad();
+        CloseHandle(hSzeregowy);
+        return;
+    }
+
+    // Budujemy wiadomosc ping - specjalna sekwencja + terminator
+    std::string pingMsg = "PING";
+    pingMsg += pobierzTerminator(uPr);
+
+    char recvBuf[256];
+    DWORD wyslane = 0, odczytane = 0;
+
+    std::cout<<"Wysylam PING...\n";
+
+    // Mierzymy czas wyslania i odebrania odpowiedzi przez QueryPerformanceCounter (wysoka rozdzielczosc)
+    LARGE_INTEGER freq, start, stop;
+    QueryPerformanceFrequency(&freq);
+    QueryPerformanceCounter(&start);
+
+    if(!WriteFile(hSzeregowy, pingMsg.c_str(), (DWORD)pingMsg.length(), &wyslane, NULL))
+    {
+        std::cout<<"BLAD: Nie udalo sie wyslac PING.\n";
+        wyswietlBlad();
+        CloseHandle(hSzeregowy);
+        return;
+    }
+
+    // Czekamy na dowolna odpowiedz
+    if(!ReadFile(hSzeregowy, recvBuf, sizeof(recvBuf)-1, &odczytane, NULL))
+    {
+        std::cout<<"BLAD: Blad odczytu odpowiedzi PING.\n";
+        wyswietlBlad();
+        CloseHandle(hSzeregowy);
+        return;
+    }
+
+    QueryPerformanceCounter(&stop);
+
+    if(odczytane < 1)
+    {
+        std::cout<<"PING: Brak odpowiedzi (timeout "<<pingTimeout<<"ms).\n";
+    }
+    else
+    {
+        // Obliczamy round trip delay w milisekundach
+        double rtt = (double)(stop.QuadPart - start.QuadPart) * 1000.0 / (double)freq.QuadPart;
+        recvBuf[odczytane] = '\0';
+        std::cout<<"PING: Odpowiedz odebrana! Round trip delay: "<<(int)rtt<<" ms\n";
+        std::cout<<"Odebrano "<<odczytane<<" bajtow: "<<recvBuf<<"\n";
+    }
+
+    CloseHandle(hSzeregowy);
+}
+
 HANDLE utworzPort(int nrPortu)
 {
     std::stringstream ss;
@@ -459,11 +712,14 @@ int ustawNrPortu(int nrPortu)
     return nrPortu;
 }
 
-int wysylanieWiadomosci(int nrPortu, DCB parametry ,COMMTIMEOUTS timeouts)
+int wysylanieWiadomosci(int nrPortu, DCB parametry, COMMTIMEOUTS timeouts, UstawieniaPort uPr)
 {
     HANDLE hSzeregowy = utworzPort(nrPortu);
     if(!hSzeregowy)
         return 1;
+
+    // Aplikujemy ustawienia kontroli przeplywu przed SetCommState
+    zastosujKontrolePrzeplywu(parametry, uPr.kontrolaPrzeplywu);
 
     if(!SetCommState(hSzeregowy, &parametry))
     {
@@ -481,7 +737,7 @@ int wysylanieWiadomosci(int nrPortu, DCB parametry ,COMMTIMEOUTS timeouts)
 
     DWORD odczytane=0;
 
-    std::cout<<"Twoja wiadomosc (maks 100 znkow): ";
+    std::cout<<"Twoja wiadomosc (maks 100 znakow): ";
     std::stringstream wiadomosc;
     std::string buff;
     std::cin>>buff;
@@ -489,9 +745,12 @@ int wysylanieWiadomosci(int nrPortu, DCB parametry ,COMMTIMEOUTS timeouts)
     std::getline(std::cin,buff);
     wiadomosc<<buff;
 
+    // Dolaczamy terminator do wiadomosci przed wyslaniem
+    std::string doWyslania = wiadomosc.str() + pobierzTerminator(uPr);
+
     //Czy cstr ma ta sama dlugosc co str.length? -> znak /0 uwzgledniony?
-    //if(!WriteFile(hSzeregowy,(odwrocZnaki(wiadomosc.str())).c_str(), wiadomosc.str().length(), &odczytane, NULL))     //WriteFile robi zapis
-    if(!WriteFile(hSzeregowy,(wiadomosc.str()).c_str(), wiadomosc.str().length(), &odczytane, NULL))     //WriteFile robi zapis
+    //if(!WriteFile(hSzeregowy,(odwrocZnaki(doWyslania)).c_str(), doWyslania.length(), &odczytane, NULL))     //WriteFile robi zapis
+    if(!WriteFile(hSzeregowy, doWyslania.c_str(), (DWORD)doWyslania.length(), &odczytane, NULL))     //WriteFile robi zapis
     {
         std::cout<<"BLAD: Nie udalo sie wyslac wiadomosci\n";
         wyswietlBlad();
@@ -502,7 +761,7 @@ int wysylanieWiadomosci(int nrPortu, DCB parametry ,COMMTIMEOUTS timeouts)
     return 0;
 }
 
-int nasluchujWiadomosci(int nrPortu, DCB parametry ,COMMTIMEOUTS timeouts)
+int nasluchujWiadomosci(int nrPortu, DCB parametry, COMMTIMEOUTS timeouts, UstawieniaPort uPr)
 {
     HANDLE hSzeregowy = utworzPort(nrPortu);
     if(!hSzeregowy)
@@ -510,6 +769,9 @@ int nasluchujWiadomosci(int nrPortu, DCB parametry ,COMMTIMEOUTS timeouts)
 
     char buff[1000];
     DWORD odczytane=0;
+
+    // Aplikujemy ustawienia kontroli przeplywu przed SetCommState
+    zastosujKontrolePrzeplywu(parametry, uPr.kontrolaPrzeplywu);
 
     if(!SetCommState(hSzeregowy, &parametry))
     {
@@ -592,12 +854,12 @@ COMMTIMEOUTS ustawieniaCzasowOczekiwania(COMMTIMEOUTS timeouts)
     return timeouts;
 }
 
-void wysylanieCiagleWiadomosci(int nrPortu, DCB dcbParamSzereg, COMMTIMEOUTS timeouts)
+void wysylanieCiagleWiadomosci(int nrPortu, DCB dcbParamSzereg, COMMTIMEOUTS timeouts, UstawieniaPort uPr)
 {
     bool wysylamy = true;
     while(wysylamy)
     {
-        if(wysylanieWiadomosci(nrPortu, dcbParamSzereg, timeouts))
+        if(wysylanieWiadomosci(nrPortu, dcbParamSzereg, timeouts, uPr))
             break;
         std::cout<<"Wysyłamy dalej? (Y/N): ";
         char wyb;
@@ -607,18 +869,30 @@ void wysylanieCiagleWiadomosci(int nrPortu, DCB dcbParamSzereg, COMMTIMEOUTS tim
     }
 }
 
-void nasluchujCiagleWiadomosci(int nrPortu, DCB dcbParamSzereg, COMMTIMEOUTS timeouts)
+void nasluchujCiagleWiadomosci(int nrPortu, DCB dcbParamSzereg, COMMTIMEOUTS timeouts, UstawieniaPort uPr)
 {
-    bool wysylamy = true;
-    while(wysylamy)
+    // Zamiast pytac po kazdym timeoucie, uzywamy kbhit() do sprawdzania czy uzytkownik wcisnal klawisz
+    // Wyjscie: nacisniec 'q' lub 'Q' (nie blokuje petli odbioru)
+    std::cout<<"Tryb ciaglego odbioru. Nacisnij 'q' + Enter zeby wyjsc.\n";
+
+    // Osobny watek nie jest potrzebny - kbhit() sprawdza czy jest znak w buforze klawiatury
+    // bez blokowania. Jesli nie - natychmiast wraca.
+    while(true)
     {
-        if(nasluchujWiadomosci(nrPortu, dcbParamSzereg, timeouts))
-            break;
-        std::cout<<"Nasluchujemy dalej? (Y/N): ";
-        char wyb;
-        std::cin>>wyb;
-        if (wyb == 'N' || wyb == 'n' || wyb == '0')
-            wysylamy=false;
+        // Sprawdzamy czy uzytkownik chce wyjsc (kbhit jest z conio.h - zawsze dostepne na Windows)
+        if(_kbhit())
+        {
+            char k = (char)_getch();
+            if(k == 'q' || k == 'Q')
+            {
+                std::cout<<"\nWychodzenie z trybu odbioru...\n";
+                break;
+            }
+        }
+
+        // Probujemy odebrac wiadomosc (z timeoutem ustawionym przez uzytkownika)
+        nasluchujWiadomosci(nrPortu, dcbParamSzereg, timeouts, uPr);
+        // Jesli timeout - po prostu lecimy dalej i znowu sprawdzamy klawiature
     }
 }
 
@@ -627,6 +901,13 @@ int main( int argc, char** argv)
     DCB dcbParamSzereg = {0};
     COMMTIMEOUTS timeouts = {0};
     int nrPortu=1;
+
+    // Domyslne ustawienia terminatora i kontroli przeplywu
+    UstawieniaPort uPr;
+    uPr.terminatorTyp     = TERM_CRLF;  // domyslnie CR-LF
+    uPr.terminatorZnak1   = 0;
+    uPr.terminatorZnak2   = 0;
+    uPr.kontrolaPrzeplywu = FLOW_BRAK;  // domyslnie bez kontroli przeplywu
 
     if( wczytajUstawienia(dcbParamSzereg, timeouts))
     {
@@ -652,16 +933,27 @@ int main( int argc, char** argv)
         std::cout<<"Obecny numer portu: COM"<<nrPortu<<'\n';
         wyswietlObecneUstawienia(dcbParamSzereg);
         std::cout<<"Obecne timeouty: "; wyswietlObecneTimeouty(timeouts);
+        std::cout<<"Terminator: "; wyswietlTerminator(uPr); std::cout<<"\n";
+        std::cout<<"Kontrola przeplywu: "; wyswietlKontrolePrzeplywu(uPr); std::cout<<"\n";
 
         char wyb;
-        std::cout<<"\nWybierz co chcesz zrobic: \n \t 0-wyjsc z programu 1-Ustawic port 2-Ustawienia transmisji \n \
-        3-Ustawienia czasow oczekiwania 4-wyslac wiadomosc 5-odebrac wiadomosc\n"
-        <<"6-Wysylac wiadomosci 7-Odbierac wiadomosci\n";
+        std::cout<<"\nWybierz co chcesz zrobic: \n"
+                 <<"\t e-wyjsc z programu\n"
+                 <<"\t 1-Ustawic port\n"
+                 <<"\t 2-Ustawienia transmisji\n"
+                 <<"\t 3-Ustawienia czasow oczekiwania\n"
+                 <<"\t 4-Wyslac wiadomosc\n"
+                 <<"\t 5-Odebrac wiadomosc\n"
+                 <<"\t 6-Wysylac wiadomosci (ciagle)\n"
+                 <<"\t 7-Odbierac wiadomosci (ciagle, wyjscie: q+Enter)\n"
+                 <<"\t 8-Ustawienia terminatora\n"
+                 <<"\t 9-Ustawienia kontroli przeplywu\n"
+                 <<"\t 0-PING\n";
         std::cout<<"\\********************************\n";
         std::cout<<"Wybor>";
         std::cin>>wyb;
 
-        if(wyb=='0')
+        if(wyb=='e' || wyb=='E')
             maDzialac=false;
         if(wyb=='1')
             nrPortu = ustawNrPortu(nrPortu);
@@ -670,13 +962,19 @@ int main( int argc, char** argv)
         if(wyb=='3')
             timeouts=ustawieniaCzasowOczekiwania(timeouts);
         if(wyb=='4')
-            wysylanieWiadomosci(nrPortu,dcbParamSzereg, timeouts);
+            wysylanieWiadomosci(nrPortu, dcbParamSzereg, timeouts, uPr);
         if(wyb=='5')
-            nasluchujWiadomosci(nrPortu,dcbParamSzereg, timeouts);
+            nasluchujWiadomosci(nrPortu, dcbParamSzereg, timeouts, uPr);
         if(wyb=='6')
-            wysylanieCiagleWiadomosci(nrPortu,dcbParamSzereg, timeouts);
+            wysylanieCiagleWiadomosci(nrPortu, dcbParamSzereg, timeouts, uPr);
         if(wyb=='7')
-            nasluchujCiagleWiadomosci(nrPortu,dcbParamSzereg, timeouts);
+            nasluchujCiagleWiadomosci(nrPortu, dcbParamSzereg, timeouts, uPr);
+        if(wyb=='8')
+            uPr = ustawTerminator(uPr);
+        if(wyb=='9')
+            uPr = ustawKontrolePrzeplywu(uPr);
+        if(wyb=='0')
+            ping(nrPortu, dcbParamSzereg, timeouts, uPr);
     }
     return 0;
 
